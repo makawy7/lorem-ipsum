513479d9767e7291f9942873fe525e3c4f045ab1
abdallah <abdallahelmekawy6@gmail.com>
2023-02-21 01:52:04 +0200

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState, useRef } from "react";
 import text from "./data";
 
 function App() {
+  const [lorem, setLorem] = useState([]);
+  const inputRef = useRef(null);
+  const generate = (e) => {
+    e.preventDefault();
+    const count =
+      inputRef.current.value >= text.length
+        ? text.length
+        : inputRef.current.value;
+    setLorem(text.slice(0, count));
+  };
   return (
     <>
-      <h1>App</h1>
+      <section className="section-center">
+        <h3>tired of boring lorem ipsum?</h3>
+        <form className="lorem-form">
+          <label htmlFor="amount">paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            min={0}
+            max={text.length}
+            defaultValue={0}
+            ref={inputRef}
+          />
+          <button className="btn" onClick={generate}>
+            generate
+          </button>
+        </form>
+        <article className="lorem-text">
+          {lorem.map((l, idx) => (
+            <p key={idx}>{l}</p>
+          ))}
+        </article>
+      </section>
     </>
   );
 }
